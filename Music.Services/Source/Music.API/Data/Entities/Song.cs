d92ab6dd21fa2248215306339c87fa7bb5949aad
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2023-04-07 23:19:00 +0530

    Update Song.cs

--- Music.Services/Source/Music.API/Data/Entities/Song.cs
+++ Music.Services/Source/Music.API/Data/Entities/Song.cs
@@ -1,7 +1,10 @@
-﻿namespace Music.API.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music.API.Data.Entities;
 
 public class Song
 {
+    [Key]
     public Guid Id { get; set; }
 
     public string? Title { get; set; }
