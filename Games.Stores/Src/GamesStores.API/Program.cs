519007950ba5909d859da4c4f7622794d2a0d7e6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-14 01:39:42 +0530

    Update Program.cs

--- Games.Stores/Src/GamesStores.API/Program.cs
+++ Games.Stores/Src/GamesStores.API/Program.cs
@@ -1,6 +1,11 @@
+using GamesStores.API.Core.Interfaces;
 using GamesStores.API.Endpoints;
+using GamesStores.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Please use /Swagger to get details on GamesStores.API.");
