d1a373edeca385de10a92b272a61b2e3e97392e7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-14 00:23:32 +0530

    Swamy/13may work (#7)
    * Initial Version
    
    * Initial Version
    
    * Update launchSettings.json
    
    * Entity Class
    
    * Update Program.cs
    
    * Folder Structure
    
    * Update Program.cs
    
    * Code Refactoring
    
    * Update Program.cs
    
    * Update Program.cs
    
    * Added Postman Collections
    
    * Code Refactoring
    
    * Reorganization
    
    * Data Anotations
    
    * Entity Validations

---
+++ Games.Stores/Src/GamesStores.API/Core/Common/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace GamesStores.API.Core.Common;
+
+public static class Constants
+{
+
+    public static class GameEndpointNames
+    {
+        public static string GetGameByIdName => "GetGameById";
+    }
+
+    public static class GameEndpointRoutes
+    {
+        public static string Prefix => "/games";
+
+        public static string Root => "/";
+
+        public static string ActionById => "/{id}";
+    }
+}
---
+++ Games.Stores/Src/GamesStores.API/Data/Entities/Game.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesStores.API.Data.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string? Name { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string? Genre { get; set; }
+
+    [Required]
+    [Range(1, 100)]
+    public decimal? Price { get; set; }
+
+    public DateTimeOffset? ReleaseDate { get; set; }
+
+    [Url]
+    [StringLength(150)]
+    public string? ImageUri { get; set; }
+}
