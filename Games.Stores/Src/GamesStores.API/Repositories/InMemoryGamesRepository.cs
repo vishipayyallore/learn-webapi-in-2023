f912e01fb9f059743bf86e6185dada9f5b61b451
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-14 01:06:09 +0530

    Update InMemoryGamesRepository.cs

--- Games.Stores/Src/GamesStores.API/Repositories/InMemoryGamesRepository.cs
+++ Games.Stores/Src/GamesStores.API/Repositories/InMemoryGamesRepository.cs
@@ -4,12 +4,7 @@ namespace GamesStores.API.Repositories;
 
 public class InMemoryGamesRepository
 {
-    static readonly List<Game> games = new()
-    {
-        new Game { Id = 1, Name = "Street Fight II", Genre ="Fighting", Price = 18.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
-        new Game { Id = 2, Name = "Final Fantasy XIV", Genre ="Roleplaying", Price = 19.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
-        new Game { Id = 3, Name = "FIFA 2023", Genre ="Sports", Price = 20.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" }
-    };
+    private readonly List<Game> games = GetDummyGames();
 
     public IReadOnlyCollection<Game> GetAllGames()
     {
@@ -42,4 +37,13 @@ public class InMemoryGamesRepository
         games.RemoveAt(index);
     }
 
+    private static List<Game> GetDummyGames()
+    {
+        return new List<Game>()
+        {
+            new Game { Id = 1, Name = "Street Fight II", Genre ="Fighting", Price = 18.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
+            new Game { Id = 2, Name = "Final Fantasy XIV", Genre ="Roleplaying", Price = 19.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
+            new Game { Id = 3, Name = "FIFA 2023", Genre ="Sports", Price = 20.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" }
+        };
+    }
 }
