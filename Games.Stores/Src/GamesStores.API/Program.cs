2bd9bed4c5b01e56bce784ecf22dbc71a00c88c1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-13 22:51:13 +0530

    Update Program.cs

--- Games.Stores/Src/GamesStores.API/Program.cs
+++ Games.Stores/Src/GamesStores.API/Program.cs
@@ -1,6 +1,18 @@
+
+using GamesStores.API.Entities;
+
+List<Game> games = new()
+{
+    new Game { Id = 1, Name = "Street Fight II", Genre ="Fighting", Price = 18.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
+    new Game { Id = 2, Name = "Final Fantasy XIV", Genre ="Roleplaying", Price = 19.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
+    new Game { Id = 3, Name = "FIFA 2023", Genre ="Sports", Price = 20.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Please use /Swagger to get details on GamesStores.API.");
+
+app.MapGet("/games", () => games);
 
 app.Run();
