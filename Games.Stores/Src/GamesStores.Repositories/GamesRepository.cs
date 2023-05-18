0984e6c7b54f9e4e5f7db8a1233ca7acd61e708b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-18 14:42:02 +0530

    EFCore Repository

--- Games.Stores/Src/GamesStores.Repositories/GamesRepository.cs
+++ Games.Stores/Src/GamesStores.Repositories/GamesRepository.cs
@@ -1,50 +1,46 @@
 ﻿using GamesStores.ApplicationCore.Interfaces;
 using GamesStores.Data.Entities;
+using GamesStores.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace GamesStores.Repositories;
 
 public class GamesRepository : IGamesRepository
 {
-    private readonly List<Game> games = GetDummyGames();
+    private readonly GamesStoreDbContext _gamesStoreDbContext;
+
+    public GamesRepository(GamesStoreDbContext gamesStoreDbContext)
+    {
+        _gamesStoreDbContext = gamesStoreDbContext ?? throw new ArgumentNullException(nameof(gamesStoreDbContext));
+    }
 
     public IReadOnlyCollection<Game> GetAllGames()
     {
-        return games;
+        return _gamesStoreDbContext.Games.AsNoTracking().ToList();
     }
 
     public Game? GetGameById(int id)
     {
-        return games.Find(game => game.Id == id);
+        return _gamesStoreDbContext.Games.Find(id);
     }
 
     public void CreateGame(Game game)
     {
-        game.Id = games.Max(game => game.Id) + 1;
+        _gamesStoreDbContext.Games.Add(game);
 
-        games.Add(game);
+        _gamesStoreDbContext.SaveChanges();
     }
 
     public void UpdateGame(Game updatedGame)
     {
-        var index = games.FindIndex(game => game.Id == updatedGame.Id);
-
-        games[index] = updatedGame;
+        _gamesStoreDbContext.Update(updatedGame);
     }
 
     public void DeleteGame(int id)
     {
-        var index = games.FindIndex(game => game.Id == id);
-
-        games.RemoveAt(index);
+        _gamesStoreDbContext.Games
+            .Where(game => game.Id == id)
+            .ExecuteDelete();
     }
 
-    private static List<Game> GetDummyGames()
-    {
-        return new List<Game>()
-        {
-            new Game { Id = 1, Name = "Street Fight II", Genre ="Fighting", Price = 18.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
-            new Game { Id = 2, Name = "Final Fantasy XIV", Genre ="Roleplaying", Price = 19.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" },
-            new Game { Id = 3, Name = "FIFA 2023", Genre ="Sports", Price = 20.00M, ReleaseDate = DateTime.Now, ImageUri = "https://placehold.co/100" }
-        };
-    }
 }
