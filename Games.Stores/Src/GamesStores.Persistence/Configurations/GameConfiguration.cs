e258f62cd197d933b6d11e47f15da898c2a8aba0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-18 13:48:40 +0530

    Game Configuration

---
+++ Games.Stores/Src/GamesStores.Persistence/Configurations/GameConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using GamesStores.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GamesStores.Persistence.Configurations;
+
+public class GameConfiguration : IEntityTypeConfiguration<Game>
+{
+
+    public void Configure(EntityTypeBuilder<Game> builder)
+    {
+        builder.Property<decimal?>(game => game.Price)
+            .HasPrecision(5, 2);
+    }
+
+}
--- Games.Stores/Src/GamesStores.Persistence/GamesStoreDbContext.cs
+++ Games.Stores/Src/GamesStores.Persistence/GamesStoreDbContext.cs
@@ -1,13 +1,20 @@
 ﻿using GamesStores.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace GamesStores.Persistence;
 
 public class GamesStoreDbContext : DbContext
 {
+    public DbSet<Game> Games => Set<Game>();
+
     public GamesStoreDbContext(DbContextOptions<GamesStoreDbContext> options) : base(options)
     {
     }
 
-    public DbSet<Game> Games => Set<Game>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
 }
