4d923857a0a6aab2a3dc772b59b16e3226f843f0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-16 18:13:15 +0530

    Code Refactoring

---
+++ SportPlusShop/SportPlusShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportPlusShop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+    [HttpGet]
+    public string GetAllProducts()
+    {
+        return "OK";
+    }
+
+}
