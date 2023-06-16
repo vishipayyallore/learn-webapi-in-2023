f0b5e08044ba2a20175b7b42491175ac5f5690cf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-16 19:28:53 +0530

    Create HttpRequestPipelineExtensions.cs

---
+++ SportPlusShop/SportPlusShop.API/Extensions/HttpRequestPipelineExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace SportPlusShop.API.Extensions;
+
+public static class HttpRequestPipelineExtensions
+{
+
+    public static WebApplication ConfigureHttpRequestPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapGet("/", () => "Please use /swagger to see the SportPlusShop.API documentation.");
+
+        return app;
+    }
+}
