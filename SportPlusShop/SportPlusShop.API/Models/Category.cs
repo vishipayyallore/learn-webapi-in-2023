ba770af1adf06a404d0fa754e406083c043b9773
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-16 18:21:27 +0530

    Added Models

---
+++ SportPlusShop/SportPlusShop.API/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace SportPlusShop.API.Models
+{
+
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public virtual List<Product>? Products { get; set; }
+    }
+
+}
