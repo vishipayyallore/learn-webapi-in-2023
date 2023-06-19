0f69b54c16bd663097a5c18d0587ee5320f9aee9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-19 16:09:38 +0530

    Update ProductQueryParameters.cs

--- SportPlusShop/SportPlusShop.API/Queries/ProductQueryParameters.cs
+++ SportPlusShop/SportPlusShop.API/Queries/ProductQueryParameters.cs
@@ -5,4 +5,9 @@ public class ProductQueryParameters : QueryParameters
     public decimal? MinPrice { get; set; }
 
     public decimal? MaxPrice { get; set; }
+
+    public string? Sku { get; set; }
+
+    public string? Name { get; set; }
+
 }
