58e6292555dc5976d39d58346c7642e8dab00ccb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-19 17:28:26 +0530

    Filtering and Searching

--- SportPlusShop/SportPlusShop.API/Models/Category.cs
+++ SportPlusShop/SportPlusShop.API/Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace SportPlusShop.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportPlusShop.API.Models;
 
 public class Category
 {
     public Guid Id { get; set; }
 
+    [Required]
     public string? Name { get; set; }
 
     public virtual List<Product>? Products { get; set; }
--- SportPlusShop/SportPlusShop.API/Models/Product.cs
+++ SportPlusShop/SportPlusShop.API/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace SportPlusShop.API.Models;
 
@@ -6,16 +7,20 @@ public class Product
 {
     public Guid Id { get; set; }
 
+    [Required]
     public string? Sku { get; set; }
 
+    [Required]
     public string? Name { get; set; }
 
+    [Required]
     public string? Description { get; set; }
 
     public decimal Price { get; set; }
 
     public bool IsAvailable { get; set; }
 
+    [Required]
     public Guid CategoryId { get; set; }
 
     [JsonIgnore]
