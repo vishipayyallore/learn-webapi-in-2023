49cdb837ffacae25dfde4cd098eb7f6bfb674915
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-20 11:31:56 +0530

    Sort

--- SportPlusShop/SportPlusShop.API/Extensions/IQueryableExtensions.cs
+++ SportPlusShop/SportPlusShop.API/Extensions/IQueryableExtensions.cs
@@ -9,12 +9,12 @@ public static class IQueryableExtensions
         var type = typeof(TEntity);
         var expression2 = Expression.Parameter(type, "t");
         var property = type.GetProperty(sortBy);
-        var expression1 = Expression.MakeMemberAccess(expression2, property);
+        var expression1 = Expression.MakeMemberAccess(expression2, property!);
         var lambda = Expression.Lambda(expression1, expression2);
         var result = Expression.Call(
             typeof(Queryable),
             sortOrder == "desc" ? "OrderByDescending" : "OrderBy",
-            new Type[] { type, property.PropertyType },
+            new Type[] { type, property!.PropertyType },
             items.Expression,
             Expression.Quote(lambda));
 
--- SportPlusShop/SportPlusShop.API/Queries/QueryParameters.cs
+++ SportPlusShop/SportPlusShop.API/Queries/QueryParameters.cs
@@ -4,6 +4,7 @@ public class QueryParameters
 {
     const int _maxSize = 100;
     private int _size = 50;
+    private string _sortOrder = "asc";
 
     public int Page { get; set; }
 
@@ -12,5 +13,20 @@ public class QueryParameters
         get => _size;
 
         set => _size = Math.Min(_maxSize, value);
+    }
+
+    public string? SortBy { get; set; }
+
+    public string SortOrder
+    {
+        get => _sortOrder;
+
+        set
+        {
+            if (value is "asc" or "desc")
+            {
+                _sortOrder = value;
+            }
+        }
     }
 }
