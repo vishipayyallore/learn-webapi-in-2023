0056e297aa1f4dd45427dfb7346eb618f6009bf2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-21 23:03:52 +0530

    Swamy/21sep shirtsshop s1 day1 (#25)
    * Create ShirtsShop.sln
    
    * Initial Version
    
    * Added API
    
    * Update Program.cs
    
    * Update Program.cs

---
+++ ShirtsShop/ShirtsShop.Api/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/shirts", () =>
+{
+    return "Reading all the shirts.";
+});
+
+app.MapGet("/shirts/{id}", (int id) =>
+{
+    return $"Reading shirt with id: {id}.";
+});
+
+app.MapPost("/shirts", () =>
+{
+    return "Creating a shirt.";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"Updating shirt with id: {id}.";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting shirt with id: {id}.";
+});
+
+app.Run();
+
