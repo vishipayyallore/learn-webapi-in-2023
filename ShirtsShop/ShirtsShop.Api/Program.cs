8ff935d02760c4763c80e9b9c688bcfa2eecdc08
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-21 23:03:01 +0530

    Update Program.cs

--- ShirtsShop/ShirtsShop.Api/Program.cs
+++ ShirtsShop/ShirtsShop.Api/Program.cs
@@ -26,6 +26,20 @@ app.MapGet("/shirts/{id}", (int id) =>
     return $"Reading shirt with id: {id}.";
 });
 
+app.MapPost("/shirts", () =>
+{
+    return "Creating a shirt.";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"Updating shirt with id: {id}.";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting shirt with id: {id}.";
+});
 
 app.Run();
 
